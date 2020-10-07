4d00b2707be4c93f6f04c734d064e801f2b4f58f
bytesavvie <luckystan0525@gmail.com>
2020-10-07 19:05:09 +0200

    Use Apollo Client for data fetching

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { LightTheme, BaseProvider } from 'baseui';
 import React from 'react';
 import { Provider as StyletronProvider } from 'styletron-react';
@@ -8,14 +9,21 @@ import Layout from '../components/layout';
 // eslint-disable-next-line import/no-unassigned-import
 import '../css/global.css';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
+
 const MyApp = ({ Component, pageProps }) => (
-  <StyletronProvider debugAfterHydration value={styletron} debug={debug}>
-    <BaseProvider theme={LightTheme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </BaseProvider>
-  </StyletronProvider>
+  <ApolloProvider client={client}>
+    <StyletronProvider debugAfterHydration value={styletron} debug={debug}>
+      <BaseProvider theme={LightTheme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </BaseProvider>
+    </StyletronProvider>
+  </ApolloProvider>
 );
 
 export default MyApp;
--- pages/club/[id].js
+++ pages/club/[id].js
@@ -1,15 +1,37 @@
 import { Heading } from 'baseui/heading';
 import { useRouter } from 'next/router';
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
 
 const Club = () => {
   const router = useRouter();
   const { id } = router.query;
 
+  const { loading, error, data } = useQuery(
+    gql`
+      query GetClub($id: ID!) {
+        club(id: $id) {
+          id
+          name
+          description
+        }
+      }
+    `,
+    {
+      variables: { id },
+    }
+  );
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Errored!</p>;
+  if (!data?.club) return <p>Not found!</p>;
+
+  const { club } = data;
+
   return (
     <>
-      <Heading>{id}</Heading>
-      <div>Description</div>
+      <Heading>{club.name}</Heading>
+      <div>{club.description}</div>
     </>
   );
 };
