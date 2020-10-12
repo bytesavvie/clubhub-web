774c3ca64b4375a47e2e018ba3314f6ccc3341c4
bytesavvie <luckystan0525@gmail.com>
2020-10-12 12:50:14 +0200

    Use AppNavBar instead of HeaderNavigation

--- components/header.js
+++ components/header.js
@@ -1,45 +1,55 @@
-import { Button } from 'baseui/button';
-import {
-  HeaderNavigation,
-  ALIGN,
-  StyledNavigationList,
-  StyledNavigationItem,
-} from 'baseui/header-navigation';
+import { AppNavBar, setItemActive } from 'baseui/app-nav-bar';
 import React from 'react';
+import { ArrowRight, ChevronDown, Delete, Plus, Upload } from 'baseui/icon';
 
-import Container from './container';
 import Link from './link';
 import { useUser } from '../utilities/auth/use-user';
+import { useRouter } from 'next/router';
 
 const Header = () => {
+  const router = useRouter();
   const { user, logout } = useUser();
 
+  const [mainItems, setMainItems] = React.useState([
+    { icon: Upload, label: 'Main A' },
+    {
+      icon: ChevronDown,
+      label: 'Main B',
+      navExitIcon: Delete,
+      children: [
+        { icon: Upload, label: 'Secondary A' },
+        { icon: Upload, label: 'Secondary B' },
+      ],
+    },
+  ]);
+
+  const userItems = [
+    { icon: Plus, label: 'Create Club', info: { href: '/club/create' } },
+    { icon: ArrowRight, label: 'Logout', info: { logout: true } },
+  ];
+
+  const Title = <Link href="/">ClubHub</Link>;
+
+  const handleMenuItemSelect = (item) =>
+    setMainItems((previous) => setItemActive(previous, item));
+
+  const handleUserItemSelect = ({ info }) =>
+    info.href
+      ? // eslint-disable-next-line functional/immutable-data
+        router.push(info.href)
+      : info.logout
+      ? logout()
+      : null;
+
   return (
-    <HeaderNavigation>
-      <Container innerStyle={{ display: 'flex' }}>
-        <StyledNavigationList $align={ALIGN.left}>
-          <StyledNavigationItem $style={{ paddingLeft: 0 }}>
-            <Link href="/">ClubHub</Link>
-          </StyledNavigationItem>
-        </StyledNavigationList>
-        <StyledNavigationList $align={ALIGN.center}>
-          <StyledNavigationItem>
-            <Link href="/club/create">Create Club</Link>
-          </StyledNavigationItem>
-        </StyledNavigationList>
-        <StyledNavigationList $align={ALIGN.right}>
-          <StyledNavigationItem>
-            {user ? (
-              <Button onClick={() => logout()}>Logout</Button>
-            ) : (
-              <Link unstyled href="/auth">
-                <Button>Login</Button>
-              </Link>
-            )}
-          </StyledNavigationItem>
-        </StyledNavigationList>
-      </Container>
-    </HeaderNavigation>
+    <AppNavBar
+      title={Title}
+      mainItems={mainItems}
+      username={user && user.email}
+      userItems={userItems}
+      onMainItemSelect={handleMenuItemSelect}
+      onUserItemSelect={handleUserItemSelect}
+    />
   );
 };
 
